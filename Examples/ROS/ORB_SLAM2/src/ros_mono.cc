b8a7adce3db9a62f7b38a2cd05892469eb1b22cb
EmilDuleba <e.duleba@xberry.tech>
2017-10-19 19:27:42 +0200

    Mono example publishing poses via ROS node. Additional input parameters.

--- Examples/ROS/ORB_SLAM2/src/ros_mono.cc
+++ Examples/ROS/ORB_SLAM2/src/ros_mono.cc
@@ -25,8 +25,10 @@
 #include<chrono>
 
 #include<ros/ros.h>
+#include <geometry_msgs/Pose.h>
 #include <cv_bridge/cv_bridge.h>
-
+#include <Eigen/Core>
+#include <Eigen/Geometry>
 #include<opencv2/core/core.hpp>
 
 #include"../../../include/System.h"
@@ -41,6 +43,7 @@ public:
     void GrabImage(const sensor_msgs::ImageConstPtr& msg);
 
     ORB_SLAM2::System* mpSLAM;
+    ros::Publisher pose_publisher;
 };
 
 int main(int argc, char **argv)
@@ -48,21 +51,22 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "Mono");
     ros::start();
 
-    if(argc != 3)
+    if(argc != 6)
     {
-        cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings" << endl;        
+        cerr << endl << "Usage: rosrun ORB_SLAM2 Mono path_to_vocabulary path_to_settings camera_topic pose_topic perview" << endl;        
         ros::shutdown();
         return 1;
     }    
 
+	std::string preview = argv[5];
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR,true);
+    ORB_SLAM2::System SLAM(argv[1],argv[2],ORB_SLAM2::System::MONOCULAR, preview == "true");
 
     ImageGrabber igb(&SLAM);
 
     ros::NodeHandle nodeHandler;
-    ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb);
-
+    ros::Subscriber sub = nodeHandler.subscribe(argv[3], 1, &ImageGrabber::GrabImage,&igb); // "/camera/rgb/image_color"
+    igb.pose_publisher = nodeHandler.advertise<geometry_msgs::Pose>(argv[4], 10); // "/slam/pose"
     ros::spin();
 
     // Stop all threads
@@ -90,7 +94,41 @@ void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
         return;
     }
 
-    mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+    cv::Mat pose = mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+
+    if (pose.empty())
+        return;
+
+    /* global left handed coordinate system */
+    static cv::Mat pose_prev = cv::Mat::eye(4,4, CV_32F);
+    static cv::Mat world_lh = cv::Mat::eye(4,4, CV_32F);
+    // matrix to flip signs of sinus in rotation matrix, not sure why we need to do that
+    static const cv::Mat flipSign = (cv::Mat_<float>(4,4) <<   1,-1,-1, 1,
+                                     -1, 1,-1, 1,
+                                     -1,-1, 1, 1,
+                                     1, 1, 1, 1);
+
+    //prev_pose * T = pose
+    cv::Mat translation =  (pose * pose_prev.inv()).mul(flipSign);
+    world_lh = world_lh * translation;
+    pose_prev = pose.clone();
+
+    Eigen::Matrix3f m;
+    m << pose.at<float>(0,0), pose.at<float>(0,1), pose.at<float>(0,2),
+         pose.at<float>(1,0), pose.at<float>(1,1), pose.at<float>(1,2),
+         pose.at<float>(2,0), pose.at<float>(2,1), pose.at<float>(2,2);
+	Eigen::Quaternionf q(m);
+
+	geometry_msgs::Pose pose_msg;
+    pose_msg.position.x = world_lh.at<float>(0,3);
+	pose_msg.position.y = world_lh.at<float>(1,3);
+	pose_msg.position.z = -world_lh.at<float>(2,3);
+	pose_msg.orientation.x = q.x();
+    pose_msg.orientation.y = q.y();
+    pose_msg.orientation.z = q.z();
+    pose_msg.orientation.w = q.w();
+	
+	pose_publisher.publish(pose_msg);
 }
 
 
